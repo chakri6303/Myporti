0468ae82d15d86aec4d154ecec30c6a63ef3247b
chakri6303 <138191999+chakri6303@users.noreply.github.com>
2024-12-24 00:00:25 +0530

    Update Footer.jsx

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -88,9 +88,9 @@ const Footer = () => {
         <Nav>
           <NavLink href="#About">About</NavLink>
           <NavLink href="#Skills">Skills</NavLink>
-          {/* <NavLink href="#Experience">Experience</NavLink> */}
+          <NavLink href="#Experience">Experience</NavLink>
           <NavLink href="#Projects">Projects</NavLink>
-          <NavLink href="#Education">Education</NavLink>
+{/*           <NavLink href="#Education">Education</NavLink> */}
         </Nav>
         <SocialMediaIcons>
           {/* <SocialMediaIcon href={Bio.facebook} target="display">
